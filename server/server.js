20c5c0d8195e53492c00f2cae226437ce21e4e74
Oliver Hyryläinen <itzkinkkuu@gmail.com>
2022-02-10 08:57:58 +0200

    pls work

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const app = express();
 const publicPath = path.join(__dirname, "../build");
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
